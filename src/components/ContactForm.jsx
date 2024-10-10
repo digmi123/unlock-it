97ff3d42d80a26d95eee27a46798563612558be2
digmi123 <65775879+digmi123@users.noreply.github.com>
2024-10-10 19:44:55 +0300

    Add Contact Form.

---
+++ src/components/ContactForm.jsx
@@ -0,0 +1,52 @@
+import { useRef } from "react";
+
+export default function ContactForm() {
+  const formRef = useRef(null);
+
+  return (
+    <div className="p-6 bg-primary rounded-md flex flex-col justify-between">
+      <h2 className="text-4xl text-center font-bold">
+        Contact Us for a Free Service Estimate
+      </h2>
+      <form ref={formRef} className="flex flex-col gap-4">
+        <input
+          type="text"
+          name="user_name"
+          className="px-4 py-3 border-input rounded-md"
+          placeholder="Name*"
+        />
+        <input
+          type="email"
+          name="user_email"
+          className="px-4 py-3 border-input rounded-md"
+          placeholder="Email*"
+        />
+        <input
+          type="number"
+          name="phone_number"
+          className="px-4 py-3 border-input rounded-md"
+          placeholder="Phone Number*"
+        />
+        <textarea
+          name="message"
+          className="px-4 py-3 border-input rounded-md"
+          placeholder="Message*"
+        />
+        <button
+          type="submit"
+          value="Send"
+          className="bg-white px-4 py-3 rounded-full text-primary font-bold uppercase"
+        >
+          Send
+        </button>
+      </form>
+
+      <div className="flex flex-col items-center gap-2">
+        <p className="text-2xl">
+          Or call us at: <span className="font-bold">(03) 6358 2037</span>
+        </p>
+        <p>(23,447 Clients)</p>
+      </div>
+    </div>
+  );
+}
