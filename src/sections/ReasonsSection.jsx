de39d6a36810b95fb4d51f67e09b86dca5e07208
digmi123 <65775879+digmi123@users.noreply.github.com>
2024-10-11 03:51:12 +0300

    Add ContactLayout to reasons section.

--- src/sections/ReasonsSection.jsx
+++ src/sections/ReasonsSection.jsx
@@ -1,11 +1,10 @@
-import ContactForm from "../components/ContactForm";
+import ContactLayout from "@/Layouts/ContactLayout";
 import LockedOut from "../components/LockedOut";
 
 export default function ReasonsSection() {
   return (
-    <section className="px-52 py-12 border-2 border-black grid grid-cols-[repeat(auto-fit,_minmax(400px,_1fr))] gap-6">
+    <ContactLayout>
       <LockedOut />
-      <ContactForm />
-    </section>
+    </ContactLayout>
   );
 }
