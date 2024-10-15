410fb2f5e058a732a4417e1bb7170f7d955e7086
digmi123 <65775879+digmi123@users.noreply.github.com>
2024-10-15 23:56:01 +0300

    Add Unlock Section and a divider.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import FAQ from "./sections/FAQ";
 import Help from "./sections/Help";
 import ReasonsSection from "./sections/ReasonsSection";
+import UnlockSection from "./sections/UnlockSection";
 
 function App() {
   return (
     <>
+      <UnlockSection />
+      <div className="w-[90vw] h-[2px] bg-slate-300 my-12 mx-auto rounded-md" />
       <ReasonsSection />
       <FAQ />
       <Help />
