03b93d56f3b696e6eeab45404dccd45ad3498b64
digmi123 <65775879+digmi123@users.noreply.github.com>
2024-10-28 02:17:08 +0200

    Update GuaranteeBanner.

--- src/components/guaranteeBanner/GuaranteeBanner.jsx
+++ src/components/guaranteeBanner/GuaranteeBanner.jsx
@@ -1,21 +1,25 @@
-import React from 'react'
-import guaranteeImage from '../../assets/Gur.png'
-import { CallUsButton } from '@/main/common'
+import guaranteeImage from "../../assets/Gur.png";
+import { CallUsButton } from "@/main/common";
 function GuaranteeBanner() {
   return (
-    <div className="bg-black py-10 px-5">
-    <div className="flex justify-center  md:flex-row items-center m-auto max-w-7xl mx-auto">
+    <div className="py-10 px-5 flex justify-around items-center flex-wrap">
       <div className="mb-4 md:mb-0">
-        <img className='h-80' src={guaranteeImage} alt="Guarantee Illustration" />
+        <img
+          className="h-80"
+          src={guaranteeImage}
+          alt="Guarantee Illustration"
+        />
       </div>
-      <div className="text-white w-1/2 text-start md:text-left m-6 p-6 md:ml-6">
+
+      <div className="text-primary w-1/2 text-start md:text-left p-6">
         <p className="text-2xl font-semibold pb-6">Our Guarantee</p>
-        <h2 className="text-5xl font-bold pb-6">Top Quality Service & Cheapest Rate</h2>
-      <CallUsButton/>
+        <h2 className="text-5xl font-bold pb-6">
+          Top Quality Service & Cheapest Rate
+        </h2>
+        <CallUsButton />
       </div>
     </div>
-  </div>
-  )
+  );
 }
 
-export default GuaranteeBanner
+export default GuaranteeBanner;
