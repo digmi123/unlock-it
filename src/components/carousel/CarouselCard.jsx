da6a8c45aeabe3ffd03ad4f630b1cfa14d27a4b0
digmi123 <65775879+digmi123@users.noreply.github.com>
2024-10-28 02:13:57 +0200

    Remove spacings.

--- src/components/carousel/CarouselCard.jsx
+++ src/components/carousel/CarouselCard.jsx
@@ -1,6 +1,5 @@
-import React from "react";
 import { CarouselPlugin } from "./Carousel";
-import {customerData} from './const'
+import { customerData } from "./const";
 import { CallUsButton } from "@/main/common";
 // Sample data
 
@@ -9,19 +8,17 @@ const CarouselCard = () => {
     <div className="w-full flex flex-col items-center mb-20">
       <h2 className="text-center text-6xl font-bold mb-8 mt-8">
         Top Rated in Melbourne, Over 23,000+ Happy Clients.
-
       </h2>
-      
+
       <div className="w-full">
         <CarouselPlugin dataCustom={customerData} />
       </div>
       <div className="flex flex-col items-center justify-center text-center">
-
-      <h3 className="text-center text-4xl font-bold mb-8 mt-8">Join the Happy Clients List. Call Now for a Free Estimate</h3>
-      <CallUsButton/>
+        <h3 className="text-center text-4xl font-bold mb-8 mt-8">
+          Join the Happy Clients List. Call Now for a Free Estimate
+        </h3>
+        <CallUsButton />
       </div>
-      
-      
     </div>
   );
 };
