d6467f9c1e57e025891cdb68cb99d0ee5cad09f5
digmi123 <65775879+digmi123@users.noreply.github.com>
2024-11-03 18:41:47 +0200

    Update call us button component.

--- src/main/common/components/call-us-button/CallUsButton.jsx
+++ src/main/common/components/call-us-button/CallUsButton.jsx
@@ -4,8 +4,13 @@ import { cn } from "@/lib/utils";
 
 const CallUsButton = ({ isDisplayText = false, className }) => {
   return (
-    <a href={`tel:${PHONE_NUMBER}`} className="w-full">
-      <Button className={cn("w-full py-8 px-12 text-3xl rounded-full max-w-[520px]", className)}>
+    <a href={`tel:${PHONE_NUMBER}`} className="w-fit">
+      <Button
+        className={cn(
+          "w-full py-8 px-12 text-3xl rounded-full max-w-[520px]",
+          className
+        )}
+      >
         {isDisplayText ? `Call Now ${PHONE_NUMBER}` : PHONE_NUMBER}
       </Button>
     </a>
